02e07e2eb18cdfcdad817f3555c80caed3296d40
Aseem Shrey <aseem.shrey@grofers.com>
2020-02-02 19:17:38 +0530

    Added middlewares and JWT authentication

---
+++ fullstack/api/middlewares/middlewares.go
@@ -0,0 +1,26 @@
+package middlewares
+
+import (
+	"net/http"
+	"github.com/LuD1161/posts_api/fullstack/api/auth"
+)
+
+// SetMiddleWareJSON : Set response header to application/json header
+func SetMiddleWareJSON(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next(w, r)
+	}
+}
+
+// SetMiddleWareAuthentication : Check for authenticated users
+func SetMiddleWareAuthentication(next http.HandlerFunc) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		err := auth.TokenValid(r)
+		if err != nil{
+			responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+			return
+		}
+		next(w, r)
+	}
+}
